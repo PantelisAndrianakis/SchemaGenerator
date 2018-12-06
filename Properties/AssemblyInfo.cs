377170425b2310066fd7d2bdadb5bbfb98cd0e11
Pantelis Andrianakis <pantelisandrianakis@gmail.com>
2018-12-06 15:21:26 +0200

    Initial commit.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XSD Schema Generator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XSD Schema Generator")]
+[assembly: AssemblyCopyright("Copyright © Pantelis Andrianakis 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa7a1400-4cc3-43e1-b309-c2b7c69ec4c1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,9 @@
+# XSD Schema Generator
+A console application to generate an XSD schema based on several XML files.
+
+## Syntax example
+SchemaGenerator schema.xsd 1.xml 2.xml 3.xml 4.xml 5.xml
+
+### .NET Framework compatibility
+- You need to have .NET Framework 4 to run this.<br>
+- Made with Microsoft Visual Studio 2017 Community edition.
